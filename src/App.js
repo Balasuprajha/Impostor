b365a0b58f4ba8920e7b88f3e2adc2739b8b3a8d
Balasuprajha <139677495+Balasuprajha@users.noreply.github.com>
2026-04-06 12:24:45 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -58,9 +58,7 @@ function App() {
       reconnectionDelay: 1000,
       reconnectionDelayMax: 5000,
       reconnectionAttempts: 5,
-      transports: ['websocket', 'polling'],
-      pingInterval: 25000,
-      pingTimeout: 20000,
+      transports: ['polling'],
     });
 
     newSocket.on('connect', () => {
